53ee93df27d2f02cc985d77e27a319251538d12c
maxwell-kizito <osisiomakizito@gmail.com>
2024-09-02 20:58:24 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('').click(function(){
+        $(this).toggleClass('fa-times');
+    });
+
+    $(window).on('scroll load',function(){
+
+        $('.fa-bars').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+    
+    });
+
+    if($(window).scrollTop() >30){
+        $('header').addClass('header-active');
+    }
+    else{
+        $('header').removeClass('header-active');
+    }
+
+});
+
+
+
